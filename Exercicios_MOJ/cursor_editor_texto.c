120dab193764517498600579da7d6fc75eb511c9
phmelosilva <pedrodsm1819@gmail.com>
2026-06-09 08:11:57 -0300

    inicia exercicio cursor no editor de texto

--- Exercicios_MOJ/cursor_editor_texto.c
+++ Exercicios_MOJ/cursor_editor_texto.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    
+    int l = 0;
+    int linha, coluna = 1;
+    char texto[1005][1005];
+
+    scanf("%d", &l);
+    getchar();
+    for (int i = 0; i < l; i++) {
+        fgets(texto[i], 1005, stdin);
+    }
+    scanf("%d %d", &linha, &coluna);
+    linha--;
+    coluna--;
+    int coluna_alvo = coluna;
+    char comando;
+    while (scanf(" %c", &comando) != EOF) {
+        if (comando == 'j') {
+            if (linha < l - 1) {
+                linha++;
+
+                if ((strlen(texto[linha]) - 2 )) {}
+            }
+        }
+
+        if (comando == 'k') {
+            if (linha < l -1) {
+
+            }
+        }
+    }
+
+
 }
